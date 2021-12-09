990ab7f4c1830676c42f91733a55a36c0641b6fe
Diogo Bastos <bastos.diogo@gmail.com>
2021-12-09 21:43:05 +0000

    Added part 2

--- src/009/part_two.py
+++ src/009/part_two.py
@@ -1,8 +1,24 @@
 import common
 
 
-def get_basin(smoke_map, x, y):
+def get_basin(smoke_map, x, y, visited=[]):
     point = int(smoke_map[y][x])
+    visited.append((x, y))
+
+    if point == 9:
+        return []
+
+    result = [(x, y)]
+    if x - 1 >= 0 and (x - 1, y) not in visited:
+        result += get_basin(smoke_map, x - 1, y, visited=visited)
+    if x + 1 < len(smoke_map[0]) and (x + 1, y) not in visited:
+        result += get_basin(smoke_map, x + 1, y, visited=visited)
+    if y - 1 >= 0 and (x, y - 1) not in visited:
+        result += get_basin(smoke_map, x, y - 1, visited=visited)
+    if y + 1 < len(smoke_map) and (x, y + 1) not in visited:
+        result += get_basin(smoke_map, x, y + 1, visited=visited)
+
+    return result
 
 
 if __name__ == "__main__":
@@ -21,8 +37,13 @@ if __name__ == "__main__":
                     continue
 
                 basin = get_basin(smoke_map, x, y)
-                print("Basin size at", x, y, "is", len(basin))
-                basins.append(basin)
+                basins.append((len(basin), basin))
 
-        print(basins)
-        break
+                for bx, by in basin:
+                    smoke_map[by] = smoke_map[by][:bx] + "9" + smoke_map[by][bx + 1:]
+
+        basins = sorted(basins)[::-1]
+        one, two, three = basins[0][0], basins[1][0], basins[2][0]
+        result = one * two * three
+
+        print(f"For {input_file} largest basins are {one}, {two} and {three} and result is {result}")
